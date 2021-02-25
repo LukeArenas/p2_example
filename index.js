c3008c81c0a4443c4590d3b22d0906caad94e2b4
Andre Pato <anpato1994@gmail.com>
2021-02-25 17:23:15 -0500

    update middleware

--- index.js
+++ index.js
@@ -14,14 +14,13 @@ const PORT = process.env.PORT || 3001
 app.use(cors())
 app.use(bodyParser.json())
 app.use(logger('dev'))
+app.use('/users', UserRouter)
+app.use('/todos', TodoRouter)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')))
   app.get('*', (req, res) => {
     res.sendFile(path.join(`${__dirname}/client/build/index.html`))
   })
 }
-
-app.use('/users', UserRouter)
-app.use('/todos', TodoRouter)
 
 app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`))
